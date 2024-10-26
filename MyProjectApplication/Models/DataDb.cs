e47922c7ccdba84b4fde1086ca4ba7b7f69f80f2
Choffel <daniilmiadzve@gmail.com>
2024-10-26 17:44:12 +0300

    add-new-model

--- MyProjectApplication/Models/DataDb.cs
+++ MyProjectApplication/Models/DataDb.cs
@@ -7,5 +7,9 @@ namespace MyProjectApplication.Models
         public DataDb(DbContextOptions<DataDb> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        
+        public DbSet<User> Users { get; set; }
+        
+        public DbSet<Role> Roles { get; set; }
     }
 }
--- MyProjectApplication/Models/Product.cs
+++ MyProjectApplication/Models/Product.cs
@@ -6,7 +6,6 @@ public class Product
     
     public int Id { get; set; }
     
-    
     public double Price { get; set; }
     
 }
